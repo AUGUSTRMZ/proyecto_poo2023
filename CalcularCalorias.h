0de0103bf6a4850c1d7026f61dff1958b743da63
AUGUSTRMZ <142357102+AUGUSTRMZ@users.noreply.github.com>
2023-11-26 23:13:45 -0600

    Update CalcularCalorias.h

--- CalcularCalorias.h
+++ CalcularCalorias.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 
+//defino la clase
 class CalcularCalorias {
 public:
   static float kcal(const Paciente &paciente);
@@ -11,6 +12,7 @@ float CalcularCalorias::kcal(const Paciente &paciente) {
   float factorSexo = (paciente.getSexo() == 'M') ? 5.0 : 1.8;
   int calorias;
 
+  //establezco una condicional para que entrege un resultado diferente si es hombre o mujer
   if (paciente.getSexo() == 'M') {
     calorias = 10 * paciente.getPeso() + 6.25 * paciente.getAltura() -
                5 * paciente.getEdad() + 5;
@@ -18,6 +20,6 @@ float CalcularCalorias::kcal(const Paciente &paciente) {
     calorias = 10 * paciente.getPeso() + 6.25 * paciente.getAltura() -
                5 * paciente.getEdad() - 161;
   }
-
+//multiplica las calorias por el factor sexo
   return calorias * factorSexo;
 }
